1d26bc03474e35719112cc400f9947385638939d
andresdcgt <dwandres05@gmail.com>
2021-09-10 20:14:32 -0600

    Comito primero

---
+++ src/main/java/com/wtorresd1/Facturass/FacturassApplication.java
@@ -0,0 +1,13 @@
+package com.wtorresd1.Facturass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtorresd1/Facturass/FacturassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtorresd1.Facturass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
